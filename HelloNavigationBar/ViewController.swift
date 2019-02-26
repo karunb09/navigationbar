b213830edc193dd8f40fae5654c29cb8421ec59c
Bourishetty,Karun <s533900@ch140007m.local>
2019-02-26 11:02:13 -0600

    Initial Commit

---
+++ HelloNavigationBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloNavigationBar
+//
+//  Created by Bourishetty,Karun on 2/21/19.
+//  Copyright © 2019 Bourishetty,Karun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
